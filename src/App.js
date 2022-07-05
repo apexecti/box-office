89703bdd575979cc530ab4d37df1abf25f066fac
apexecti <paulparthib2002@gmail.com>
2022-07-05 13:50:08 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 
 function App() {
   return (
@@ -14,6 +15,11 @@ function App() {
       <Route exact path="/starred">
         <Starred />
       </Route>
+
+      <Route exact path="/show/:id">
+        <Show />
+      </Route>
+
       <Route>
         <div>Not found</div>
       </Route>
